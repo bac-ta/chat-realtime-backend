eaeb6f0ff347ead6f93ab865aa15bce03add3d18
bac-ta <bac93.it@gmail.com>
2020-08-21 16:05:36 +0700

    update source code

--- src/main/java/com/dimagesharevn/app/repositories/UserRepository.java
+++ src/main/java/com/dimagesharevn/app/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.dimagesharevn.app.repositories;
 
 import com.dimagesharevn.app.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
