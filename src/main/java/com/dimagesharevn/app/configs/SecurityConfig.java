98afe4629eb393115b8154c1185cbcdbf55aa3cd
bac-ta <bac93.it@gmail.com>
2020-09-11 10:31:30 +0700

    update source

--- src/main/java/com/dimagesharevn/app/configs/SecurityConfig.java
+++ src/main/java/com/dimagesharevn/app/configs/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/auth/login").permitAll()
+                .antMatchers("/auth/login", "/user/create").permitAll()
                 .anyRequest()
                 .authenticated();
         // Add our custom JWT security filter
