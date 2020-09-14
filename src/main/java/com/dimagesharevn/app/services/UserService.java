5a3ddea474d93a9b002e52c77e051249876b151e
bac-ta <bac93.it@gmail.com>
2020-09-14 16:46:22 +0700

    update source

--- src/main/java/com/dimagesharevn/app/services/UserService.java
+++ src/main/java/com/dimagesharevn/app/services/UserService.java
@@ -22,10 +22,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
