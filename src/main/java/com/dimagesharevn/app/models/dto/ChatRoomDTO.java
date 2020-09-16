e4604f1b274fcf4e0a5b2b6fa833bc400adce6f2
bac-ta <bac93.it@gmail.com>
2020-09-16 16:18:59 +0700

    update chat service

---
+++ src/main/java/com/dimagesharevn/app/models/dto/ChatRoomDTO.java
@@ -0,0 +1,14 @@
+package com.dimagesharevn.app.models.dto;
+
+import com.dimagesharevn.app.models.rests.request.ChatRoomRequest;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ChatRoomDTO extends ChatRoomRequest {
+    private boolean persistent = true;
+    private boolean publicRoom = true;
+}
---
+++ src/main/java/com/dimagesharevn/app/models/dto/MemberDTO.java
@@ -0,0 +1,12 @@
+package com.dimagesharevn.app.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDTO {
+    private String member;
+}
