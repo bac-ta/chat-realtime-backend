4a93643715c282760826bb87b6fe571fde46ac10
SozNgx <kiwitinh@gmail.com>
2020-09-23 14:10:14 +0700

    update

--- src/main/java/com/dimagesharevn/app/utils/ResourceNotFoundExceptionHandler.java
+++ src/main/java/com/dimagesharevn/app/utils/ResourceNotFoundExceptionHandler.java
@@ -3,7 +3,9 @@ package com.dimagesharevn.app.utils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-
+/**
+ * @author bac-ta
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundExceptionHandler extends RuntimeException {
     public ResourceNotFoundExceptionHandler(String message) {
